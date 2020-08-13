d850bd1bf2ac4caa28fa23db85fbc3563af542be
ordanax <ordanax@ya.ru>
2020-08-13 04:07:42 +0300

    Add files via upload

---
+++ 3wm_v_3/polybar/scripts/flux.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ click-left ]; then
+        killall -s 6 xflux && xflux -l 56.34, -g 18.95 -k 2000
+if [ click-right ]; then
+        killall -s 6 xflux && xflux -l 56.34, -g 18.95 -k 2000
+    else
+        echo "пипа";
+    fi
