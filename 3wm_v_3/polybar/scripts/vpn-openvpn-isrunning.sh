7af5b35fa7a5a2ace4c558f4abd4e60b802c0783
ordanax <ordanax@ya.ru>
2020-08-28 04:21:19 +0300

    Add files via upload

--- 3wm_v_3/polybar/scripts/vpn-openvpn-isrunning.sh
+++ 3wm_v_3/polybar/scripts/vpn-openvpn-isrunning.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
+country=$(protonvpn s | grep Country)
 connection=$(pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1)
 
 if [ -n "$connection" ]; then
-    echo "vpn  on"
+    echo "vpn  on" $country
 else 
     echo "vpn  off"
 fi
