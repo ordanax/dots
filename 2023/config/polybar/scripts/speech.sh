638dca456b4bff323ad9dc108b9320dbd1eeacf9
ordanax <ordanax@ya.ru>
2023-05-08 03:46:52 +0300

    Delete speech.sh

--- 2023/config/polybar/scripts/speech.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Hi guys. My name is Alexey Boyko. I was born and live in Russia and I can teach you speaking Russian. I'm a businessman and work in internet marketing.
-I have my own courses where I teach people. I love teaching people something new, I know what problems students have and I know how to help them effectively.
-We will make a route for your training and you will be able to speak perfect Russian and understand what is being said. I will help you achieve your goals.
-Sign up for my trial lesson.  You will never regret it."
