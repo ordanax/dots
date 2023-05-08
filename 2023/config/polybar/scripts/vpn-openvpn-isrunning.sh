a4c567059d21bb1653d3f093c358a4cb243e0fd7
ordanax <ordanax@ya.ru>
2023-05-08 03:47:06 +0300

    Delete vpn-openvpn-isrunning.sh

--- 2023/config/polybar/scripts/vpn-openvpn-isrunning.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-country=$(protonvpn s | grep Country)
-connection=$(pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1)
-
-if [ -n "$connection" ]; then
-    echo "vpn  on" $country
-else 
-    echo "vpn  off"
-fi
